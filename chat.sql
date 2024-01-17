e068f36d4f6229ecc296a98f962fc71148ee8e81
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-01-18 03:57:28 +0800

    Prototype 3.0

--- chat.sql
+++ chat.sql
@@ -1,34 +1,138 @@
-CREATE DATABASE IF NOT EXISTS chat;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 17, 2024 at 08:54 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
-USE chat;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE users (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatrooms`
+--
+
+CREATE TABLE `chatrooms` (
+  `chatroomID` int(11) NOT NULL,
+  `chatroomName` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `chatID` int(11) NOT NULL,
+  `chatroomID` int(11) NOT NULL,
   `username` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  PRIMARY KEY (userID)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `message` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-CREATE TABLE chatrooms (
-    `chatroomID` int(11) NOT NULL,
-    `chatroomName` varchar(256) NOT NULL,
-    PRIMARY KEY (chatroomID) 
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
 
-CREATE TABLE messages (
-    `chatID` int(11) NOT NULL AUTO_INCREMENT,
-    `chatroomID` int(11) NOT NULL,
-    `username` varchar(256) NOT NULL,
-    `message` varchar(256) NOT NULL,
-    FOREIGN KEY (chatroomID) REFERENCES chatrooms(chatroomID),
-    PRIMARY KEY (chatID)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Table structure for table `users`
+--
 
-CREATE TABLE user_chatroom (
-    `chatroomID` int(11) NOT NULL,
-    `chatroomName` varchar(256) NOT NULL,
-    `username` varchar(256) NOT NULL,
-    FOREIGN KEY (chatroomID) REFERENCES chatrooms(chatroomID)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `user_chatroom`
+--
+
+CREATE TABLE `user_chatroom` (
+  `chatroomID` int(11) NOT NULL,
+  `chatroomName` varchar(256) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `is_approved` tinyint(1) DEFAULT 0,
+  `role` varchar(255) DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chatrooms`
+--
+ALTER TABLE `chatrooms`
+  ADD PRIMARY KEY (`chatroomID`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`chatID`),
+  ADD KEY `chatroomID` (`chatroomID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- Indexes for table `user_chatroom`
+--
+ALTER TABLE `user_chatroom`
+  ADD KEY `chatroomID` (`chatroomID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `chatID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`chatroomID`) REFERENCES `chatrooms` (`chatroomID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_chatroom`
+--
+ALTER TABLE `user_chatroom`
+  ADD CONSTRAINT `user_chatroom_ibfk_2` FOREIGN KEY (`chatroomID`) REFERENCES `chatrooms` (`chatroomID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
